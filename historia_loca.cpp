3e53d62582951dfbb588c210d9867f30dfb91438
Martin Sebastian Enriquez Jimenez <martin@MacBook-Air-de-Martin-3.local>
2026-02-27 18:54:20 -0500

    PROGRAMA HISTORIA LOCA

---
+++ historia_loca.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"INGRESE UN SUSTANTIVO: ";
+    string sustantivo;
+    cin>>sustantivo;
+    cout<<"\nINGRESE UN VERBO: ";
+    string verbo;
+    cin>>verbo;
+    cout<<"¡EL "<<sustantivo<<"ESTA "<<verbo<<"EN LA LUNA";
+}
